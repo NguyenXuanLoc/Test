914fa7a0a428fea5651b1182c1ca658b58321050
NguyenXuanLoc <59056264+NguyenXuanLoc@users.noreply.github.com>
2020-06-24 13:46:46 +0700

    update

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -7,6 +7,6 @@ import com.example.test.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
     }
 }
