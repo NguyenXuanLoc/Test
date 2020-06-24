1437d068b2be4a6da701f94c7c187d4313e6439f
NguyenXuanLoc <59056264+NguyenXuanLoc@users.noreply.github.com>
2020-06-24 11:29:41 +0700

    update

--- app/src/main/java/com/example/test/t6/MainActivity7.java
+++ app/src/main/java/com/example/test/t6/MainActivity7.java
@@ -3,6 +3,7 @@ package com.example.test.t6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.test.R;
 
@@ -12,5 +13,6 @@ public class MainActivity7 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main7);
+        Log.d("sd", "");
     }
 }
