d1cc069fff622f9454094992c35353f5c9afe6ec
NguyenXuanLoc <59056264+NguyenXuanLoc@users.noreply.github.com>
2020-06-24 11:28:20 +0700

    update

---
+++ app/src/main/java/com/example/test/t6/MainActivity7.java
@@ -0,0 +1,16 @@
+package com.example.test.t6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.test.R;
+
+public class MainActivity7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main7);
+    }
+}
